2a34219b7bb987cc92ee64ffee550bcfcd67dd87
Tevfik <tevfiktzn@gmail.com>
2024-08-13 12:24:59 +0300

    Update yenib.py

--- yenib.py
+++ yenib.py
@@ -6,21 +6,32 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
+
+
 @bot.event
 async def on_ready():
     print(f'{bot.user} olarak giriş yaptık')
-
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
-
+@bot.command()
+async def çöpler(ctx):
+    with open(r'm2l1\m2l2\geri-donusum-kutulari.jpg', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
 @bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
-
+@bot.command()
+async def mem(ctx):
+    with open(r'm2l1\Unh1OwPF_400x400.jpg', 'rb') as f:
+        # Dönüştürülen Discord kütüphane dosyasını bu değişkende saklayalım!
+        picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
 @bot.command(name='bot')
 async def _bot(ctx):
     """Is the bot cool?"""
     await ctx.send('Evet, bot çok havalı.')
 
-bot.run("token")
+bot.run("TOKEN")
